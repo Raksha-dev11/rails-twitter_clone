335c1453f9212e59099643c8cce4bda87e59c1f2
raksha-dev11 <raksha@drupaltechie.com>
2026-06-12 16:52:02 +0530

    added styling and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "pages/home"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,5 +13,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "pages#home"
 end
